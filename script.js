b1bc63ba9c7efdd9164de9514cd788129d99410e
Nikoloz-Kurtanidze <nikolozi.kurtanidze.1@btu.edu.ge>
2026-07-02 20:50:47 +0400

    final

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ let lat
 let lng
 let country_code
 let city
+let elevation
 
 function getLocation(){
     navigator.geolocation.getCurrentPosition(
         (position) => {
             lat = position.coords.latitude;
             lng = position.coords.longitude;
-            //lat = 40.6667 //41.7955 //48.87869 //40.6667
-            //lng = -73.9539 //45.0165 //2.3374 //-73.9539
             updateInfo()
         },
         (error) => {
@@ -22,11 +21,15 @@ document.getElementById("location_access").addEventListener("click", ()=>{
 })
 
 async function updateInfo(){
+    localStorage.setItem("lat", lat)
+    localStorage.setItem("lng", lng)
+
     let err = await updateLocation()
     if (err === 0){
         updateCountry()
         updateCity()
-        updateWeather()
+        await updateWeather()
+        updateOtherInfo()
     } else{
         alert("დაფიქსირდა შეცდომა")
     }
@@ -59,6 +62,7 @@ async function updateLocation(){
             city = data.village
             container.querySelector("#city_name").textContent = data.village
         }
+        container.style.display = "flex"
 
         return 0
 
@@ -84,6 +88,8 @@ async function updateCountry() {
         container.querySelector("#calling_code").textContent = "სატელეფონო კოდი: " + data.callingCodes[0]
         container.querySelector("#timezone").textContent = "timezone: " + data.timezones[0]
         container.querySelector("img").src =  data.flags.png
+        
+        container.style.display = "flex"
 
     } catch (error){
         console.log(error)
@@ -116,6 +122,7 @@ async function updateCity() {
         else {
             container.querySelector("img").style.visibility = "hidden"
         }
+        container.style.display = "flex"
         
     } catch (error){
         console.log(error)
@@ -156,4 +163,93 @@ async function updateWeather() {
     container.querySelector("#temperature").textContent = data.current_weather.temperature + data.current_weather_units.temperature
     container.querySelector("#weather").textContent = map[data.current_weather.weathercode].text
     container.querySelector("#icon").textContent = map[data.current_weather.weathercode].icon
+    container.querySelector("#windspeed").textContent = "ქარის სიჩქარე: " + data.current_weather.windspeed + " " + data.current_weather_units.windspeed
+    container.querySelector("#winddirection").textContent = "ქარის მიმართულება: " + data.current_weather.winddirection + data.current_weather_units.winddirection
+
+    elevation = data.elevation
+    container.style.display = "flex"
 }
+
+async function updateOtherInfo() {
+    const response = await fetch(`https://air-quality-api.open-meteo.com/v1/air-quality?latitude=${lat}&longitude=${lng}&current=pm10,pm2_5,carbon_monoxide,nitrogen_dioxide,ozone,european_aqi`)
+    const json = await response.json()
+    const data = json
+    console.log(data)
+
+    const container = document.querySelector(".information")
+    container.querySelector("#elevation").textContent = "სიმაღლე ზღვის დონიდან: " + elevation + " მ"
+    container.querySelector("#co").textContent = "ნახშირბადის მონოქსიდი: " + data.current.carbon_monoxide + data.current_units.carbon_monoxide
+    container.querySelector("#aqi").textContent = "ევროპული საჰაერო ხარისხის ინდექსი: " + data.current.european_aqi + data.current_units.european_aqi
+    container.querySelector("#nd").textContent = "აზოტის დიოქსიდი: " + data.current.nitrogen_dioxide + data.current_units.nitrogen_dioxide
+    container.querySelector("#ozone").textContent = "ოზონი: " + data.current.ozone + data.current_units.ozone
+    container.querySelector("#pm10").textContent = "მტვრის ნაწილაკები (PM10): " + data.current.pm10 + data.current_units.pm10
+    container.querySelector("#pm2_5").textContent = "მტვრის ნაწილაკები (PM2.5): " + data.current.pm2_5 + data.current_units.pm2_5
+
+    container.style.display = "flex"
+}
+
+
+const menu_icon = document.querySelector(".menu_icon")
+const hidden_menu = document.querySelector(".hidden_menu")
+
+menu_icon.addEventListener("click", ()=>{
+    if (hidden_menu.style.visibility === "hidden"){
+        hidden_menu.style.visibility = "visible"
+    } else{
+        hidden_menu.style.visibility = "hidden"
+    }
+})
+
+const scroll_btn = document.querySelector(".scroll_btn")
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 300) {
+    scroll_btn.style.visibility = "visible";
+  } else {
+    scroll_btn.style.visibility = "hidden";
+  }
+});
+
+scroll_btn.addEventListener("click", ()=>{
+    window.scrollTo({ top: 0, behavior: "smooth" })
+})
+
+function scrollToSection(className){
+    const div = document.querySelector(className);
+    div.scrollIntoView({ behavior: "smooth", block: "center" });
+}
+
+async function getCoordinates(cityName) {
+    const url = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(cityName)}&format=json&limit=1`
+
+    try {
+        const response = await fetch(url, {
+            headers: {
+                'User-Agent': 'nikolozi.kurtanidze.1@btu.edu.ge' 
+            }
+        });
+        const data = await response.json()
+
+        console.log(data)
+        lat = data[0].lat
+        lng = data[0].lon
+        updateInfo()
+       
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+document.querySelector("#text_input").addEventListener("change", (event) =>{
+    const value = event.target.value
+    console.log(value)
+    getCoordinates(value)
+})
+
+window.addEventListener('DOMContentLoaded', () => {
+    lat = localStorage.getItem("lat")
+    lng = localStorage.getItem("lng")
+    if (lat && lng){
+        updateInfo()
+    }
+})
